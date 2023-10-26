950db821328b2fb34cc215893809a17048b29124
ddp00003 <dhyanpatel7621@gmail.com>
2023-10-26 18:08:14 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 003</h2>
+      <p>WVU ID: 800367317</p>
+      <p>Hi, I am Dhyan Patel</p>
+    
+    </div>
+  );
+}
+
+export default App;
