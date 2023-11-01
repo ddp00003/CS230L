e89f198acdeeef9fb56dd72f4b9fd6b442a9cfac
ddp00003 <dhyanpatel7621@gmail.com>
2023-11-01 18:01:41 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import logo from './logo.svg';
-import Navbar from './Navbar.js';
+import Navbar from './Navbar';
 import Card from './Card.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar /> {/* Display the Navbar component */}
+      <Navbar /> 
       <h1>CS 230L</h1>
       <h2>Section - 003</h2>
       <p>WVU ID: 800367317</p>
       <p>Hi, I am Dhyan Patel</p>
-      <Card /> {/* Display the Card component */}
+      <Card /> 
     </div>
   );
 }
