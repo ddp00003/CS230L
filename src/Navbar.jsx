c3990ab7e1cce2edcd3d88edef9117b9fef61ed0
ddp00003 <dhyanpatel7621@gmail.com>
2023-11-02 14:42:13 -0400

    Initial commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <nav class="navbar bg-dark border-bottom border-body navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
-    <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    <nav className="navbar bg-dark border-bottom border-body navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-    <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Home</a>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="#">Home</a>
     </li>
-    <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <li className="nav-item">
+    <a className="nav-link" href="#">Link</a>
     </li>
-    <li class="nav-item dropdown">
-    <a class="nav-link" href="#">contact</a>
+    <li className="nav-item dropdown">
+    <a className="nav-link" href="#">contact</a>
     </li>
     </ul>
-    <form class="d-flex" role="search">
-    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-    <button class="btn btn-outline-success" type="submit">Search</button>
+    <form className="d-flex" role="search">
+    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+    <button className="btn btn-outline-success" type="submit">Search</button>
     </form>
     </div>
     </div>
