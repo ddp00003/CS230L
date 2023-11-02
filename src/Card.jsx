495af1ec13daa19170f381ea3fc877f697b0ddab
ddp00003 <dhyanpatel7621@gmail.com>
2023-11-01 20:00:40 -0400

    Initial commit

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,25 +1,52 @@
-import React from 'react';
+import React from "react";
 
 function Card() {
   return (
-    <div className="row row-cols-1 row-cols-md-3 g-4">
-      <div className="col">
-        <div className="card h-100">
-          <img src="image-url-1.jpg" className="card-img-top" alt="Image 1" />
-          <div className="card-body">
-            <h5 className="card-title">Card title 1</h5>
-            <p className="card-text">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <div class="card text-bg-primary h-100">
+          <div class="card-body">
+            <h5 class="card-title">Card 1</h5>
+            <p class="card-text">
               This is a wider card with supporting text below as a natural
               lead-in to additional content. This content is a little bit
               longer.
             </p>
           </div>
-          <div className="card-footer">
-            <small className="text-muted">Last updated 3 mins ago</small>
+          <div class="card-footer">
+            <small class="text-body-primary">Last updated 3 mins ago</small>
           </div>
         </div>
       </div>
-      
+      <div class="col">
+        <div class="card text-bg-danger h-100">
+          <div class="card-body">
+            <h5 class="card-title">Card 2</h5>
+            <p class="card-text">
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-danger">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card text-bg-success h-100">
+          <div class="card-body">
+            <h5 class="card-title">Card 3</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-body-success ">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
